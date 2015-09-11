3234c4f7815920bedef9cc594e0e26a3e1a4dbfa
Martin Hunt <martin@gameclosure.com>
2015-09-10 21:28:41 -0700

    listen for ff-adaptor on 6001
    remove previously required SERVER/connected handshake

--- src/DebuggerProxy.js
+++ src/DebuggerProxy.js
@@ -4,56 +4,63 @@ var util = require('util');
 
 var DebuggerProxyClient = require('./DebuggerProxyClient');
 
-var Server = function(port) {
+var Server = function(clientPort, serverPort) {
   events.EventEmitter.call(this);
   this.logger = require('./logs').proxyLogger;
-  this.port = port;
-  this.clients = [];
+  this.clientPort = clientPort;
+  this.serverPort = serverPort;
+  this.servers = [];
 
   this.server = null;
   this.client = null;
 
   // Is there a client connection
   this._connected = false;
-}
+};
+
 util.inherits(Server, events.EventEmitter);
 
 Server.prototype.start = function() {
-  this.logger.info('Starting debugger proxy on port ' + this.port);
-  net.createServer(this.onClient.bind(this)).listen(this.port);
-}
+  this.logger.info('starting debugger proxy');
+  this.logger.info('  listening for phones on port', this.clientPort);
+  this.logger.info('  waiting for ff-adaptor on port', this.serverPort);
 
-Server.prototype.onClient = function(socket) {
+  net.createServer(this.onDevice.bind(this)).listen(this.clientPort);
+  net.createServer(this.onServer.bind(this)).listen(this.serverPort);
+};
+
+Server.prototype.onServer = function(socket) {
   this._connected = true;
 
-  this.logger.info('Debugger client connection');
-  var client = new DebuggerProxyClient(socket, this.logger);
-  this.clients.push(client);
+  this.logger.info('debug server connected');
+  this.server = new DebuggerProxyClient(socket);
+  this.servers.push(this.server);
 
-  client.on('serverConnected', this.serverConnected.bind(this));
-  client.on('close', this.onClose.bind(this));
-
-  if (this.server !== null) {
-    this.client = client;
+  if (this.client) {
     this.startDebugging();
   }
-}
+};
+
+Server.prototype.onDevice = function(socket) {
+  this.logger.info('phone connected');
+  this.client = new DebuggerProxyClient(socket);
+  this.emit('device-connected');
+};
 
 Server.prototype.serverConnected = function(client) {
   this.logger.info('Upgrading debugger client to server');
   this.server = client;
   this.emit('serverConnected', client);
-}
+};
 
 Server.prototype.startDebugging = function() {
   this.logger.info('Starting debugging!');
 
-  this.server.send('connected');
   this.server.socket.pipe(this.client.socket);
   this.client.socket.pipe(this.server.socket);
   this.client.on('close', this.clientDisconnected.bind(this));
   this.server.on('close', this.clientDisconnected.bind(this));
-}
+};
 
 Server.prototype.clientDisconnected = function(client) {
   if (!this._connected) {
@@ -61,26 +68,14 @@ Server.prototype.clientDisconnected = function(client) {
   }
 
   this.logger.info('Client disconnected, stopping debugging');
-  for (var i = 0, len = this.clients.length; i < len; i++) {
-    this.clients[i].destroy();
+  for (var i = 0, len = this.servers.length; i < len; i++) {
+    this.servers[i].destroy();
   }
   this.server = null;
   this.client = null;
 
   this._connected = false;
   this.emit('endDebugging');
-}
-
-Server.prototype.onClose = function(connection) {
-  this.logger.info('Connection ended, cleaning up');
-  if (connection === this.server) {
-    this.server = null;
-  }
-  if (connection === this.client) {
-    this.client = null;
-  };
-  connection.socket.destroy();
 };
-
 
 module.exports = Server;
--- src/index.js
+++ src/index.js
@@ -4,17 +4,17 @@ var logger = require('./logs').mainLogger;
 
 logger.info('Starting up...');
 
-var debuggerProxy = new DebuggerProxy(6000);
+var debuggerProxy = new DebuggerProxy(6000, 6001);
 
 var ffAdapter;
 var resetFFAdapter = function() {
   ffAdapter = new FirefoxAdapter();
   ffAdapter.startServer();
-}
+};
 
 // When a server connects to the proxy, tell the adapter to start the client connection
-debuggerProxy.on('serverConnected', function(client) {
-  logger.info('Server connected, starting the ffAdapter client');
+debuggerProxy.on('device-connected', function() {
+  logger.info('device connected, starting the ffAdapter client');
   ffAdapter.startClient();
 });
 
